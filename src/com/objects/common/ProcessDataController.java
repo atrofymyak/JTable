1b107a9aaa9122c3bcd01e7d9f5c31ad05811da3
Andriy Trofymyak <andriy_trofymyak@epam.com>
2017-01-27 10:31:15 +0200

    to json convert

--- src/com/objects/common/ProcessDataController.java
+++ src/com/objects/common/ProcessDataController.java
@@ -1,6 +1,8 @@
 package com.objects.common;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,18 +13,25 @@ import org.apache.commons.io.IOUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.internal.LinkedTreeMap;
 
 public class ProcessDataController extends HttpServlet {
 
 	private static final long serialVersionUID = 1204140343352039617L;
 
+	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		List<LinkedTreeMap<String, String>> list = new ArrayList<LinkedTreeMap<String, String>>();
+		
 		for (String line : IOUtils.readLines(request.getReader())) {
 			Gson gson = new GsonBuilder().serializeNulls().create();
-			System.out.println(line);			
-			System.out.println(gson.toJson(line));
+			System.out.println(line);						
+			list =  gson.fromJson(line, List.class);					
 		}
+		
+		System.out.println(list.size());
 	}
 
 }
