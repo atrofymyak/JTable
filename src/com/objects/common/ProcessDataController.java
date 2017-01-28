1bfacee03049d7471be4fc512f7e64cde7682f0d
andriy_trofymyak <a_22_tas@yahoo.com>
2017-01-28 22:21:32 +0200

    last code

--- src/com/objects/common/ProcessDataController.java
+++ src/com/objects/common/ProcessDataController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 import javax.servlet.ServletException;
@@ -16,7 +17,6 @@ import org.apache.commons.io.IOUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.internal.LinkedTreeMap;
 
 public class ProcessDataController extends HttpServlet {
 
@@ -25,30 +25,30 @@ public class ProcessDataController extends HttpServlet {
 	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		List<LinkedTreeMap<String, String>> list = new ArrayList<LinkedTreeMap<String, String>>();
-		
+
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+
 		for (String line : IOUtils.readLines(request.getReader())) {
 			Gson gson = new GsonBuilder().serializeNulls().create();
-			System.out.println(line);						
-			list =  gson.fromJson(line, List.class);					
+			System.out.println(line);
+			list = gson.fromJson(line, List.class);
 		}
-		
+
 		System.out.println(list.size());
-		
+
 		Vector<HashMap<String, String>> vData = new Vector<HashMap<String, String>>();
-		for(LinkedTreeMap<String, String> map : list){
+		for (Map<String, String> map : list) {
 			HashMap<String, String> hashMap = new HashMap<String, String>();
-			for(Iterator<String> it = map.keySet().iterator(); it.hasNext();){
+			for (Iterator<String> it = map.keySet().iterator(); it.hasNext();) {
 				String key = it.next();
-				
+
 				hashMap.put(key, map.get(key));
 			}
-			
+
 			vData.add(hashMap);
 		}
-		
-		System.out.println("Vector size:  " + vData.size());
+
+		System.out.println("Vector size: " + vData.size());
 	}
 
 }
